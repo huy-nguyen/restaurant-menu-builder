bfc850b0eecfcd303a11d8150c1f57a27b0d1a34
Huy Nguyen <huy@periscopic.com>
2016-07-30 23:10:14 -0700

    Substantially improve webpack bundling process
    - Allow live reload with webpack-dev-server.
    
    - Allow cleaning of output directory between builds.
    
    - Add code minification.
    
    - Set global variables that distinguish between production and dev
      builds. Example use case: react optimized build.
    
    - Integrate css into output bundle.
    
    - Allow bundling splitting to separate third-party library code from
      application code. This allows the client to cache unchanged
      third-party libraries.
    
    - Add hashing to output to enable cache busting when needed.
    
    - Allow index.html page to be generated from a template so that
      scripts/stylesheets generated by webpack can be inserted easily into
      the built version of index.html.
    
    - Split the configuration into smaller parts to allow for
      customizations.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,127 @@
 const validate = require('webpack-validator');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const parts = require('./webpack-parts');
+const pgk = require('./package.json');
 
-const config = {
-  entry: './src/ts/index.js',
-  output: {
-    filename: './dist/bundle.js'
+const PATHS = {
+  scriptEnry: path.join(__dirname, 'src', 'ts'),
+  app: path.join(__dirname, 'src'),
+  style: path.join(__dirname, 'src', 'sass', 'style.scss'),
+  cssOutput: path.join(__dirname, 'src', 'css', 'style.css'),
+  build: path.join(__dirname, 'dist')
+};
+
+const common = {
+  entry: {
+    app: PATHS.scriptEnry,
+    style: PATHS.style
   },
-  // devtool: 'eval', // This enables better blackboxing in Chrome:
-  devtool: 'source-map', // Change back to this for production
+  output: {
+    path: PATHS.build,
+    // Dev environment uses `hash` and production environment uses `chunkhash`
+    // because `chunkhash` doesn't work with Hot Module Replacement.
+    filename: '[name].[hash].js',
+    sourceMapFilename: '[file].map',
+    chunkFilename: '[hash].js'
+  },
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant menu builder',
+      template: 'src/index.hbs'
+    })
+  ],
   module: {
     loaders: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {test: /\.scss$/, loaders: ['style', 'css', 'sass']}
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        // Inline small woff files and output them below font/.
+        // Set mimetype just in case.
+        loader: 'url',
+        query: {
+          name: 'font/[hash].[ext]',
+          limit: 5000,
+          mimetype: 'application/font-woff'
+        },
+        // include: PATHS.fonts
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        loader: 'file',
+        query: {
+          name: 'font/[hash].[ext]'
+        },
+        // include: PATHS.fonts
+      },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      }
     ]
   }
 }
 
-module.exports = validate(config);
+var config;
+
+switch(process.env.npm_lifecycle_event) {
+  // Triggered when `webpack` is run with `npm run build` or `npm run stats`:
+  case 'build':
+    // Intentional fall through:
+  case 'stats':
+    config = merge(
+      common,
+      {
+        output: {
+          filename: '[name].[chunkhash].js',
+          chunkFilename: '[chunkhash].js'
+        }
+      },
+      parts.clean(PATHS.build),
+      // Enable production mode in React (no logging/warning, no `propTypes`
+      // check):
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'),
+      parts.setFreeVariable(
+        'PRODUCTION',
+        true),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: Object.keys(pgk.dependencies)
+      }),
+      parts.minify(),
+      parts.extractSASS(PATHS.style, 'chunkhash')
+    );
+    break;
+  default:
+    config = merge(common,
+      {
+        devtool: 'source-map',
+      },
+      parts.setFreeVariable(
+        'PRODUCTION',
+        false),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: Object.keys(pgk.dependencies)
+      }),
+      parts.extractSASS(PATHS.style, 'hash'),
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+      );
+    break;
+}
+
+module.exports = validate(config, {
+  quiet: true
+});
