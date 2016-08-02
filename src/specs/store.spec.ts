7af08322c4515eb5075b579fb25970d941699b3d
Huy Nguyen <huy@periscopic.com>
2016-08-02 01:14:58 -0700

    Add more tests (sync and async)
    Add new tests to make sure actions (both and async) are behaving as
    expected. More tests will be added later.

--- src/specs/store.spec.ts
+++ src/specs/store.spec.ts
@@ -1,11 +1,14 @@
 import getConfiguredStore from '../ts/getConfiguredStore';
-import * as Mocha from 'mocha';
-import {expect} from 'chai';
 
 describe('Redux store', () => {
-  it('should have correct initial state', () => {
-    const store = getConfiguredStore();
-    const storeInitialState = store.getState();
+  beforeEach(() => {
+    this.store = getConfiguredStore();
+  })
+  afterEach(() => {
+    this.store = null;
+  })
+  it('Store should have correct initial state', () => {
+    const storeInitialState = this.store.getState();
     expect(storeInitialState).to.deep.equal({
       items: [],
       fetchStatus: {
