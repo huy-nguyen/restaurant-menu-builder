67ed9f571f641039c8198e4aaca0b06b673eb9a5
Huy Nguyen <huy@periscopic.com>
2016-08-02 22:08:04 -0700

    Add separate webpack config for test
    Changes from previous webpack config used in test:
    
    - The test now happens much faster. Webpack does not process SASS files
      and the `index.hbs` file anymore. TypeScript does not perform type
      checking during test, which is OK because type checking is always
      continuously performed during development.
    
    - `redux-logger` doesn't log out messages anymore during test. This was
      quite annoying in the past.

---
+++ webpack.test.config.js
@@ -0,0 +1,50 @@
+const validate = require('webpack-validator');
+const merge = require('webpack-merge');
+const parts = require('./webpack-parts');
+const pgk = require('./package.json');
+
+const PATHS = parts.PATHS;
+const nodeEnvs = parts.nodeEnvs;
+
+const NODE_ENV = nodeEnvs.test;
+
+const config = merge(
+  {
+    entry: {
+      // No style output for test.
+      app: PATHS.tsDir,
+    },
+    output: {
+      path: PATHS.buildDir,
+      filename: '[name].[chunkhash].js',
+      sourceMapFilename: '[file].map',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'ts-loader',
+          query: {
+            compilerOptions: {
+              // Disable all type checking and treat TS purely as a transpiler
+              // because any type error would have been detected during dev
+              // because type checking is alays on in dev mode:
+              noImplicitAny: false
+            }
+          }
+        }
+      ]
+    }
+  },
+  parts.commonResolution,
+  parts.commonLoaders,
+  parts.setFreeVariable(
+    'process.env.NODE_ENV',
+    NODE_ENV
+  )
+);
+
+module.exports = validate(config, {
+  quiet: true
+})
