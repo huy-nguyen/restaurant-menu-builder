290a3386ff049157edc4ea412b5a4e08c03d648c
Huy Nguyen <huy@periscopic.com>
2016-08-02 21:58:52 -0700

    Add separate config for production
    Compared to the existing production build config:
    
    - The dev stylesheet is no longer emitted along with the build.
    
    - Most third-party libraries are now loaded over a CDN instead of being
      included with the `vendor` chunk of  build. This cuts the size of the
      `vendor` chun from 390kB to 60kB and allow for client caching of these
      libraries.
    
    - Now the `process.env.NODE_ENV` has 3 possible values: 'production',
      'test' or 'development'.

---
+++ webpack.production.config.js
@@ -0,0 +1,80 @@
+const validate = require('webpack-validator');
+const merge = require('webpack-merge');
+const parts = require('./webpack-parts');
+const pgk = require('./package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = parts.PATHS;
+const nodeEnvs = parts.nodeEnvs;
+
+const NODE_ENV = nodeEnvs.production
+
+const config = merge(
+  {
+    entry: {
+      app: PATHS.tsDir,
+      productionStylesheet: PATHS.productionStylesheet
+    },
+    output: {
+      path: PATHS.buildDir,
+      publicPath: '/restaurant-menu-builder/',
+      filename: '[name].[chunkhash].js',
+      sourceMapFilename: '[file].map',
+      chunkFilename: '[chunkhash].js'
+    },
+    externals: {
+      // These libraries will be loaded from a CDN instead of be bundled in the
+      // build. E.g. whenever we `require('redux')` inside the code, that
+      // `require` call  will resolve to the variable `Redux` which has already
+      // been attached to the global object by a CDN script in the `index.html`
+      // file:
+      'jquery': 'jQuery',
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      'redux': 'Redux',
+      'lodash': '_',
+      'classnames': 'classNames',
+      'react-redux': 'ReactRedux',
+      'redux-thunk': 'ReduxThunk',
+      'underscore.string': 's'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'ts-loader',
+          exclude: PATHS.testDir
+        }
+      ]
+    }
+  },
+  parts.commonResolution,
+  parts.commonLoaders,
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Restaurant menu builder',
+        template: 'src/index.hbs',
+        chunks: ['productionStylesheet', 'manifest', 'vendor', 'app'],
+        NODE_ENV
+      })
+    ]
+  },
+  parts.clean(PATHS.buildDir),
+  // Enable production mode in React (no logging/warning, no `propTypes`
+  // check):
+  parts.setFreeVariable(
+    'process.env.NODE_ENV',
+    NODE_ENV
+  ),
+  parts.extractBundle({
+    name: 'vendor',
+    entries: Object.keys(pgk.dependencies)
+  }),
+  parts.minify(),
+  parts.extractSASS(PATHS.styleDir, 'chunkhash')
+);
+
+module.exports = validate(config, {
+  quiet: true
+})
